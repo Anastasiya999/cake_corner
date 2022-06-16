2ddec48e201a8dff22caa1a554e0d83883e150bc
Anastasiya999 <anapechko@gmail.com>
2022-06-16 20:47:56 +0200

    added style, basic components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./style/App.scss";
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import ProductCard from "./components/ProductCard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className=" content__title "> All cakes </h2>
+          <div className="content__items">
+            <ProductCard
+              title="Strawberry Cheesecake"
+              imgSrc="/img/strawberry_cheesecake.jpg"
+              price={365}
+            />
+            <ProductCard
+              title="Popcorn Unicorn"
+              imgSrc="/img/popcorn_unicorn.jpg"
+              price={245}
+            />
+            <ProductCard
+              title="Popcorn Unicorn"
+              imgSrc="/img/popcorn_unicorn.jpg"
+              price={245}
+            />
+            <ProductCard
+              title="Strawberry Lemonade"
+              imgSrc="/img/strawberry_lemonade.jpg"
+              price={245}
+            />
+            <ProductCard
+              title="Popcorn Unicorn"
+              imgSrc="/img/popcorn_unicorn.jpg"
+              price={245}
+            />
+            <ProductCard
+              title="Strawberry Lemonade"
+              imgSrc="/img/strawberry_lemonade.jpg"
+              price={245}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Categories.jsx
@@ -0,0 +1,15 @@
+function Categories() {
+  return (
+    <div class="categories">
+      <ul>
+        <li class="active">All</li>
+        <li> Vegan </li>
+        <li> Chocolate </li>
+        <li> Cheesecake </li>
+        <li> GlutenFree </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
